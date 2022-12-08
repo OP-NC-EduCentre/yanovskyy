ac24022b306fafdedf2586535a242ffadcd9905c
andre <andreyianovskyi78@gmail.com>
2022-12-08 21:41:03 +0200

    fixed 1

--- 9-AnalyticFunctions/1.sql
+++ 9-AnalyticFunctions/1.sql
@@ -30,26 +30,37 @@ CLIENTID      AVERAGE
 виділення підгруп, інша колонка повинна мати числові значення. Створіть запит, який
 отримає накопичувальні підсумки другої колонки.
 */
-SELECT
+SELECT 
     CLIENTID,
-    SUM(BILLAMOUNT) AS TOTALPAYED
-FROM
-    BILL
-GROUP BY
-    CLIENTID;
+    SUM(BILLAMOUNT) OVER (
+        PARTITION BY CLIENTID
+        ORDER BY BILLDATE
+    ) AS PAYED
+FROM BILL; 
 /*
-CLIENTID   TOTALPAYED
+CLIENTID        PAYED
 ---------- ----------
+1                 250
 1                 549
+2                 299
 2                 598
+3                 349
 3                 698
+4                 199
 4                 398
+5                 398
+5                 398
 5                 597
 6                 199
+7                 199
+7                 398
+7                 796
+7                 796
+7                 995
 7                1194
 8                 250
 
-8 rows selected.
+19 rows selected.
 */
 
 
@@ -57,25 +68,31 @@ CLIENTID   TOTALPAYED
 3. Виконайте попереднє завдання, отримавши накопичувальні підсумки в кожній
 підгрупі окремо.
 */
-SELECT
-    DISTINCT CLIENTID,
+WITH SUM_LIST AS (
+    SELECT 
+    CLIENTID,
     SUM(BILLAMOUNT) OVER (
-    PARTITION BY CLIENTID) AS TOTALPAYED
-FROM
-    BILL
-ORDER BY
-    TOTALPAYED DESC;
+        PARTITION BY CLIENTID
+        ORDER BY BILLDATE
+    ) AS PAYED
+FROM BILL
+)
+SELECT 
+    DISTINCT 
+    CLIENTID, 
+    SUM(PAYED) OVER (ORDER BY CLIENTID) AS TOTAL_BY_CLIENT
+FROM SUM_LIST; 
 /*
-CLIENTID   TOTALPAYED
----------- ----------
-7                1194
-3                 698
-2                 598
-5                 597
-1                 549
-4                 398
-8                 250
-6                 199
+CLIENTID   TOTAL_BY_CLIENT
+---------- ---------------
+1                      799
+2                     1696
+3                     2743
+4                     3340
+5                     4733
+6                     4932
+7                     9310
+8                     9560
 
 8 rows selected.
 */
